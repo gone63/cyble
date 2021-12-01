8255d7b352b29553a338e874ef7cce92395ff295
Jeevan Chhetri <jeevan.c31@gmail.com>
2021-12-01 16:16:07 +0530

    Dog list task

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
+  let [dogImages, setDogImages] = useState([]);
+  let [dogList, setDogList] = useState([]);
+
+  useEffect(() => {
+    fetch(`https://dog.ceo/api/breeds/list/all`)
+      .then((success) => success.json())
+      .then((response) => {
+        console.log("response:", response);
+
+        setDogList((_) => Object.keys(response.message));
+      });
+  }, []);
+
+  const handleChange = () => {
+    let name = document.getElementById("select-box").value;
+    fetch(`https://dog.ceo/api/breed/${name}/images`)
+      .then((success) => success.json())
+      .then((response) => {
+        console.log("response:", response);
+
+        setDogImages((_) => response.message.slice(0, 10));
+      });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <select id="select-box" onChange={handleChange}>
+        {dogList.map((name, index) => (
+          <option key={index} value={name}>
+            {name}
+          </option>
+        ))}
+      </select>
+      <div id="dog-container">
+        {dogImages.map((dog) => (
+          <img src={dog} className="dog-item" alt="image" />
+        ))}
+      </div>
     </div>
   );
 }
